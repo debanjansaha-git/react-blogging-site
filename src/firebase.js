2d399234235ed2e5ca6c56ea1dba848008ee8623
Debanjan Saha <debanjansh@gmail.com>
2024-10-19 01:02:26 -0400

    update creds

--- src/firebase.js
+++ src/firebase.js
@@ -11,13 +11,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAQg1YgI5zgi7sic-Kr7BRMZM-Kfp-RLXg",
-  authDomain: "debanjan-portfolio-site.firebaseapp.com",
-  projectId: "debanjan-portfolio-site",
-  storageBucket: "debanjan-portfolio-site.appspot.com",
-  messagingSenderId: "624463025353",
-  appId: "1:624463025353:web:e4888ce6975278cca52918",
-  measurementId: "G-TP4NHNETZR"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
